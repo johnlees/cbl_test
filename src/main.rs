a3b49c61bf6073a7c6cb653131b1c7183b826632
John Lees <lees.john6@gmail.com>
2024-02-03 18:25:49 +0000

    Use u8 type

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,13 @@ fn main() {
     // create a CBL index with parameters K and T
     let mut cbl = CBL::<K, T>::new();
 
-    let mut reader = parse_fastx_file("../ERR556974.fastq.gz").unwrap();
+    let mut reader = parse_fastx_file("ERR556974.fastq.gz").unwrap();
     // for each sequence of the FASTA/Q file
     let mut cnt = 0;
     while let Some(record) = reader.next() {
         let seqrec = record.expect("Invalid record");
         let rc = seqrec.reverse_complement();
-        for (_, kmer, _) in seqrec.canonical_kmers(K, &rc) {
+        for (_, kmer, _) in seqrec.canonical_kmers(K as u8, &rc) {
             let int_kmer = IntKmer::<K, T>::from_nucs(kmer);
             if cbl.contains(int_kmer) {
                 cnt += 1;
